66e66b4bc67c3e25dbfb00bfd44fdb325d16dea8
git config --global <dpumar3@gmail.com>
2021-09-13 22:04:56 -0500

    rutas dinamicas

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
+
+if(isset($_GET['controlador']) && isset($_GET['accion'])) {
+    $controlador = $_GET['controlador'];
+    $accion = $_GET['accion'];
+    #print_r($_GET); get es array
+    #GET prueba: http://localhost/crud/?controlador=pagina&accion=inicio
+    #print_r($controlador);
+    #print_r($accion);
+}
+
+
 require_once("vistas/template.php")
 
 ?>
--- ruteador.php
+++ ruteador.php
@@ -1,7 +1,16 @@
 <?php
 
-include_once("controladores/controlador_paginas.php");
+#datos recibidos por el get en la url 
+echo $controlador;
+echo $accion; 
 
-$controlador = new ControladorPaginas();
-$controlador->inicio();
+# crea ruta dinamica 
+include_once("controladores/controlador_".$controlador.".php");
+$objControlador="Controlador".ucfirst($controlador);
+
+#$controlador = new ControladorPaginas();
+#$controlador->inicio();
+$controlador = new $objControlador();
+$controlador->$accion();
+
 ?>
